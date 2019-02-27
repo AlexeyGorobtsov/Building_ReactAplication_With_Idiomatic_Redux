590602a8af60c2a1fd2ab5707f3226a03a1e299d
Lexa <lexa2987@mail.ru>
2019-02-27 23:16:54 +0300

    lesson_2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ import { createStore } from 'redux';
 import todoApp from './redusers';
 import App from './components/App';
 
-const store = createStore(todoApp);
+const persistedState = {
+  todos: [{
+      id: 0,
+      text: 'Welcome Back!',
+      completed: false
+  }]
+};
+
+const store = createStore(todoApp, persistedState);
 
 render(
     <Provider store={store}>
