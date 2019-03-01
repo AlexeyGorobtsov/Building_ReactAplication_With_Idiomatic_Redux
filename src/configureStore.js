97a3782be727ebd3754ddf19c5ccea8817360f6b
Lexa <lexa2987@mail.ru>
2019-03-01 19:21:17 +0300

    lesson_12

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,10 +3,28 @@ import throttle from 'lodash/throttle';
 import todoApp from './redusers';
 import {loadState, saveState} from "./localStorage";
 
+const addLoggingToDispatch = store =>{
+  const rawDispatch = store.dispatch;
+  if (!console.group) {
+      return rawDispatch;
+  }
+  return action => {
+      console.group(action.type);
+      console.log('%c prev state', 'color: gray', store.getState());
+      console.log('%c action', 'color: green', action);
+      const returnValue = rawDispatch(action);
+      console.log('%c next state', 'color: blue', store.getState());
+      console.groupEnd();
+      return returnValue;
+  }
+};
 
 const configureStore = () => {
     const persistedState = loadState();
     const store = createStore(todoApp, persistedState);
+    if (process.env.NODE_ENV !== 'production') {
+        store.dispatch = addLoggingToDispatch(store);
+    }
 
     store.subscribe(throttle(()=> {
         saveState({
