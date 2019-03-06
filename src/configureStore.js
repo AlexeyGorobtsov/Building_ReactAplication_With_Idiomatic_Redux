23d117e0f0a5b62c93a99adfdc247b348f3c4b60
Lexa <lexa2987@mail.ru>
2019-03-06 15:01:32 +0300

    lesson_18

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,41 +1,8 @@
-import {createStore} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import { createLogger } from 'redux-logger';
 
 import todoApp from './redusers';
-
-// const promise = (store) => {
-//     return (next) => {
-//         return (action) => {
-//             if (typeof action.then === 'function') {
-//                 return action.then(next)
-//             }
-//             return next(action);
-//         }
-//     }
-// };
-
-const promise = (store) => (next) => (action) => {
-    if (typeof action.then === 'function') {
-        return action.then(next);
-    }
-    return next(action);
-};
-
-const logger = store => {
-    return (next) => {
-        if (!console.group) {
-            return next;
-        }
-        return action => {
-            console.group(action.type);
-            console.log('%c prev state', 'color: gray', store.getState());
-            console.log('%c action', 'color: green', action);
-            const returnValue = next(action);
-            console.log('%c next state', 'color: blue', store.getState());
-            console.groupEnd();
-            return returnValue;
-        }
-    }
-};
 
 const wrapDispatchWithMiddlewares = (store, middlewares) =>
     middlewares.slice().reverse().forEach(middleware =>
@@ -43,15 +10,15 @@ const wrapDispatchWithMiddlewares = (store, middlewares) =>
     );
 
 const configureStore = () => {
-    const store = createStore(todoApp);
     const middlewares = [promise];
     if (process.env.NODE_ENV !== 'production') {
-        middleeares.push(logger);
+        middlewares.push(createLogger());
     }
 
-    wrapDispatchWithMiddlewares(store, middlewares);
-
-    return store;
+    return createStore(
+        todoApp,
+        applyMiddleware(...middlewares)
+    );
 };
 
 export default configureStore;
